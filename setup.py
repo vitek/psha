ef2b6afa50d5fbd4c35c6c363593da21f92cb924
Vitja Makarov <vitja.makarov@gmail.com>
2017-05-30 01:31:48 +0300

    Make setup.py work without cython compiler

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+
+have_cython = False
+try:
+    from Cython.Distutils import build_ext
+    have_cython = True
+except ImportError:
+    from distutils.command.build_ext import build_ext
+
+if have_cython:
+    psha_module = Extension('psha', ['psha.pyx'], libraries=['crypto'])
+else:
+    psha_module = Extension('psha', ['psha.c'], libraries=['crypto'])
 
 setup(
     name='SHA1 with serializable state',
-    ext_modules=cythonize([
-        Extension('psha', ['psha.pyx'], libraries=['crypto'])
-    ]),
+    ext_modules=[psha_module],
+    cmdclass={'build_ext': build_ext}
 )
